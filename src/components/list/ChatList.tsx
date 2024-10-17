d4fa63cb574195e0ea601609101298c1dca3969b
Patrick Dolan <dolanp1992@gmail.com>
2024-10-17 11:52:50 -0700

    add static ChatList component and icons

---
+++ src/components/list/ChatList.tsx
@@ -0,0 +1,74 @@
+import { useState } from "react";
+import Avatar from "../shared/Avatar";
+
+function ChatList() {
+  const [addMode, setAddMode] = useState(false);
+
+  const toggleAddMode = () => {
+    setAddMode((prev) => !prev);
+  };
+
+  return (
+    <>
+      <div className="flex items-center justify-between gap-3 p-4">
+        <div className="flex items-center gap-1 bg-slate-800 rounded-lg p-1 w-full">
+          <img src="/assets/icons/SearchIcon.svg" alt="Search" />
+          <input
+            type="text"
+            placeholder="Search"
+            className="bg-transparent w-full m-1"
+          />
+        </div>
+        <img
+          src={`${
+            addMode
+              ? "/assets/icons/RemoveIcon.svg"
+              : "/assets/icons/AddIcon.svg"
+          }`}
+          alt="Add"
+          className="bg-slate-800 p-2 rounded-lg hover:cursor-pointer"
+          onClick={toggleAddMode}
+        />
+      </div>
+      <div className="flex flex-col flex-1 overflow-y-auto">
+        <div className="flex items-center gap-4 cursor-pointer p-5 border-b border-b-black">
+          <Avatar size="sm" rounded={true} />
+          <div className="text">
+            <span className="font-medium">Jane Doe</span>
+            <p className="text-sm font-light">Hello</p>
+          </div>
+        </div>
+        <div className="flex items-center gap-4 cursor-pointer p-5 border-b border-b-black">
+          <Avatar size="sm" rounded={true} />
+          <div className="text">
+            <span className="font-medium">Jane Doe</span>
+            <p className="text-sm font-light">Hello</p>
+          </div>
+        </div>
+        <div className="flex items-center gap-4 cursor-pointer p-5 border-b border-b-black">
+          <Avatar size="sm" rounded={true} />
+          <div className="text">
+            <span className="font-medium">Jane Doe</span>
+            <p className="text-sm font-light">Hello</p>
+          </div>
+        </div>
+        <div className="flex items-center gap-4 cursor-pointer p-5 border-b border-b-black">
+          <Avatar size="sm" rounded={true} />
+          <div className="text">
+            <span className="font-medium">Jane Doe</span>
+            <p className="text-sm font-light">Hello</p>
+          </div>
+        </div>
+        <div className="flex items-center gap-4 cursor-pointer p-5 border-b border-b-black">
+          <Avatar size="sm" rounded={true} />
+          <div className="text">
+            <span className="font-medium">Jane Doe</span>
+            <p className="text-sm font-light">Hello</p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default ChatList;
