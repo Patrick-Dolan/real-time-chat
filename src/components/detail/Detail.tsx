a3d8ffadc7f9a92d7924e47a75f65fe7bd262900
Patrick Dolan <dolanp1992@gmail.com>
2024-10-21 14:15:36 -0700

    remove unnecessary classes

--- src/components/detail/Detail.tsx
+++ src/components/detail/Detail.tsx
@@ -3,14 +3,14 @@ import Avatar from "../shared/Avatar";
 function Detail() {
   return (
     <div className="w-1/4 overflow-y-auto">
-      <div className="user p-4 flex flex-col items-center gap-2 border-b border-black">
+      <div className="p-4 flex flex-col items-center gap-2 border-b border-black">
         <Avatar size="md" rounded={false} />
         <h2 className="text-lg font-bold">Jane Doe</h2>
         <p className="text-sm">Lorem ipsum dolor sit amet.</p>
       </div>
       <div className="p-4 flex flex-col gap-4">
-        <div className="option">
-          <div className="title flex justify-between">
+        <div>
+          <div className="flex justify-between">
             <span>Chat Settings</span>
             <img
               src="/assets/icons/ArrowDownIcon.svg"
@@ -19,8 +19,8 @@ function Detail() {
             />
           </div>
         </div>
-        <div className="option">
-          <div className="title flex justify-between">
+        <div>
+          <div className="flex justify-between">
             <span>Privacy & Help</span>
             <img
               src="/assets/icons/ArrowDownIcon.svg"
@@ -29,8 +29,8 @@ function Detail() {
             />
           </div>
         </div>
-        <div className="option">
-          <div className="title flex justify-between">
+        <div>
+          <div className="flex justify-between">
             <span>Shared Photos</span>
             <img
               src="/assets/icons/ArrowUpIcon.svg"
@@ -38,9 +38,9 @@ function Detail() {
               className="cursor-pointer bg-slate-800"
             />
           </div>
-          <div className="photos flex flex-col gap-4 mt-2">
-            <div className="photoitem flex items-center justify-between">
-              <div className="photodetail flex items-center gap-2">
+          <div className="flex flex-col gap-4 mt-2">
+            <div className="flex items-center justify-between">
+              <div className="flex items-center gap-2">
                 <img
                   src="https://i0.wp.com/katzenworld.co.uk/wp-content/uploads/2019/06/funny-cat.jpeg?fit=1020%2C1020&ssl=1"
                   alt=""
@@ -56,8 +56,8 @@ function Detail() {
                 className="bg-slate-800 cursor-pointer"
               />
             </div>
-            <div className="photoitem flex items-center justify-between">
-              <div className="photodetail flex items-center gap-2">
+            <div className="flex items-center justify-between">
+              <div className="flex items-center gap-2">
                 <img
                   src="https://i0.wp.com/katzenworld.co.uk/wp-content/uploads/2019/06/funny-cat.jpeg?fit=1020%2C1020&ssl=1"
                   alt=""
@@ -73,8 +73,8 @@ function Detail() {
                 className="bg-slate-800 cursor-pointer"
               />
             </div>
-            <div className="photoitem flex items-center justify-between">
-              <div className="photodetail flex items-center gap-2">
+            <div className="flex items-center justify-between">
+              <div className="flex items-center gap-2">
                 <img
                   src="https://i0.wp.com/katzenworld.co.uk/wp-content/uploads/2019/06/funny-cat.jpeg?fit=1020%2C1020&ssl=1"
                   alt=""
@@ -92,8 +92,8 @@ function Detail() {
             </div>
           </div>
         </div>
-        <div className="option">
-          <div className="title flex justify-between">
+        <div>
+          <div className="flex justify-between">
             <span>Shared Files</span>
             <img
               src="/assets/icons/ArrowDownIcon.svg"
