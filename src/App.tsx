cfb8452822b09bb6b42f5af3e0a05eba7ba2633d
Patrick Dolan <dolanp1992@gmail.com>
2024-10-22 12:09:45 -0700

    set up current user subscription and conditional logic for login

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
+import { useEffect, useState } from "react";
 import Chat from "./components/chat/Chat";
 import Detail from "./components/detail/Detail";
 import List from "./components/list/List";
 import Login from "./components/login/Login";
 import Notification from "./components/shared/Notification";
+import { onAuthStateChanged, User } from "firebase/auth";
+import { auth } from "./lib/firebase";
 
 function App() {
-  const user = false;
+  const [user, setUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    const unSub = onAuthStateChanged(auth, (user) => {
+      console.log(user);
+      setUser(user);
+    });
+    return () => {
+      unSub();
+    };
+  }, []);
 
   return (
     <div className="m-auto w-11/12 h-full bg-slate-400 dark:bg-slate-700 flex">
