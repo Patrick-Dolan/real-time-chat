cf361db8ab4f287e41944abb4abbcbbac4b8ee5c
Patrick Dolan <dolanp1992@gmail.com>
2024-10-22 10:51:11 -0700

    add uploadImage firebase function

---
+++ src/lib/uploadImage.ts
@@ -0,0 +1,35 @@
+import {
+  getStorage,
+  ref,
+  uploadBytesResumable,
+  getDownloadURL,
+} from "firebase/storage";
+
+const uploadImage = async (file: File) => {
+  const storage = getStorage();
+  const date = new Date();
+  const storageRef = ref(storage, `images/${date + file.name}`);
+
+  const uploadTask = uploadBytesResumable(storageRef, file);
+
+  return new Promise((resolve, reject) => {
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        console.log("Upload is " + progress + "% done");
+      },
+      (error) => {
+        reject("Something went wrong with image upload." + error.message);
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          resolve(downloadURL);
+        });
+      }
+    );
+  });
+};
+
+export default uploadImage;
