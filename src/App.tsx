9a8a44e2d2137eb9eb093de6db2ca4aa41ec7e74
Patrick Dolan <dolanp1992@gmail.com>
2024-10-23 12:20:42 -0700

    refactor to use zustand state management

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import Chat from "./components/chat/Chat";
 import Detail from "./components/detail/Detail";
 import List from "./components/list/List";
@@ -6,23 +6,32 @@ import Login from "./components/login/Login";
 import Notification from "./components/shared/Notification";
 import { onAuthStateChanged, User } from "firebase/auth";
 import { auth } from "./lib/firebase";
+import { useUserStore } from "./lib/userStore";
 
 function App() {
-  const [user, setUser] = useState<User | null>(null);
+  const { currentUser, isLoading, fetchUserInfo } = useUserStore();
 
   useEffect(() => {
-    const unSub = onAuthStateChanged(auth, (user) => {
-      console.log(user);
-      setUser(user);
+    const unSub = onAuthStateChanged(auth, (user: User | null) => {
+      fetchUserInfo(user?.uid ?? null);
     });
     return () => {
       unSub();
     };
-  }, []);
+  }, [fetchUserInfo]);
+
+  if (isLoading)
+    return (
+      <div className="flex justify-center items-center h-full">
+        <h1 className="text-4xl font-bold px-16 py-8 bg-slate-700 rounded-lg">
+          Loading...
+        </h1>
+      </div>
+    );
 
   return (
     <div className="m-auto w-11/12 h-full bg-slate-400 dark:bg-slate-700 flex">
-      {user ? (
+      {currentUser ? (
         <>
           <List />
           <Chat />
